3edf6c14c9f0005ac62a8aa19abba692ea476499
Agustin <33077305+agusc01@users.noreply.github.com>
2022-04-12 16:31:31 -0300

    Testing colors on filters

--- custom-api-wakko.js
+++ custom-api-wakko.js
@@ -13,14 +13,13 @@ function ajaxCustomApiWakko(url, cFunction) {
 
 function functionColorsCustomApiWakko(xhttp) {
     let response = JSON.parse(xhttp.response);
-    //on products
-    functionColorsInProductCustomApiWakko("div a .btn-variant-content",response );
+    
+    functionColorsOnProductCustomApiWakko("div a .btn-variant-content",response );
 
-    //on filters
-    functionColorsInProductCustomApiWakko("[data-store='filters-group'] label span.checkbox",response );
+    functionColorsOnFiltersCustomApiWakko("[data-store='filters-group'] label span.checkbox",response );
 }
 
-function functionColorsInProductCustomApiWakko(selector,jsonColors){
+function functionColorsOnProductCustomApiWakko(selector,jsonColors){
     try {
         let selectorColors = document.querySelectorAll(selector);
         selectorColors.forEach(selectorColor=>{
@@ -35,3 +34,23 @@ function functionColorsInProductCustomApiWakko(selector,jsonColors){
         console.warn(error)
     }
 }
+
+function functionColorsOnFiltersCustomApiWakko(selector,jsonColors){
+    try {
+        let selectorColors = document.querySelectorAll(selector);
+        selectorColors.forEach(selectorColor=>{
+            jsonColors.forEach(jsonColor=>{
+                if(jsonColor.name == selectorColor.innerText.split(" (")[0]){
+                    selectorColor.style.backgroundColor = jsonColor.backGround;
+                    //TODO: text-color !
+                }
+            })
+        })
+    } catch (error) {
+        console.warn(error)
+    }
+}
+
+
+
+
