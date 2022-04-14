b3d0f876291ac2df5571f47d93bf30ba5b4205c7
Agustin <33077305+agusc01@users.noreply.github.com>
2022-04-14 16:20:54 -0300

    Added third banner-category

--- custom-api-wakko.js
+++ custom-api-wakko.js
@@ -63,3 +63,36 @@ function functionStayCenterPills(){
 }
 
 
+function bannerWrapper(){
+    try {
+        // let banner = document.querySelector(".banner-wrapper");
+        let categories = document.querySelectorAll(".banner-wrapper > .span6");
+        if(categories.length < 3){
+            categories.forEach(category =>{
+                category.classList.add("span4");
+                category.classList.remove("span6");
+            });
+            let newCategory = `
+                <div class="span4">
+                    <div class="textbanner text-center">
+                        <a class="banner-with-text-link" href="https://wakko.mitiendanube.com/decoracion/" title="Decoración" aria-label="Decoración">
+                            <div class="textbanner-image overlay">
+                                <img class="textbanner-image-background lazyautosizes lazyload blur-up-big" src="https://agusc01.github.io/api-wakko/assets/images/banner/12220.jpg" data-sizes="auto" data-expand="-10" alt="Decoración"> 
+                                <div class="placeholder-shine"></div>
+                            </div>
+                            <div class="textbanner-text">
+                                <div class="textbanner-title">Decoración</div>
+                                <div class="textbanner-paragraph">Productos Nacionales !</div>
+                                <div class="btn btn-primary">Ver más productos</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>`;
+            // TODO: Take off jQuery . jQuery is on tiendanube by default
+            $(".banner-wrapper").append(newCategory);
+        }
+        
+    } catch (error) {
+        console.warn(error);
+    }
+}
