1243c108361d0fe77196286224eb9d80d0aa10c9
Agustin <33077305+agusc01@users.noreply.github.com>
2022-08-16 09:32:07 -0300

    Nuevo formato de categorias, ahora hay 4

--- custom-api-wakko.js
+++ custom-api-wakko.js
@@ -70,23 +70,42 @@ function bannerWrapper(){
         let categories = document.querySelectorAll(".banner-wrapper > .span6");
         if(categories.length < 3){
             let newCategory = `
-                <div class="span4">
-                    <div class="textbanner text-center">
-                        <a class="banner-with-text-link" href="https://wakko.mitiendanube.com/decoracion/" title="Decoración" aria-label="Decoración">
-                            <div class="textbanner-image overlay">
-                                <img class="textbanner-image-background lazyautosizes lazyload blur-up-big" src="https://agusc01.github.io/api-wakko/assets/images/banner/12220.jpg" data-sizes="auto" data-expand="-10" alt="Decoración"> 
-                                <div class="placeholder-shine"></div>
-                            </div>
-                            <div class="textbanner-text">
-                                <div class="textbanner-title">Decoración</div>
-                                <div class="textbanner-paragraph">Productos Nacionales !</div>
-                                <div class="btn btn-primary">Ver más productos</div>
-                            </div>
-                        </a>
+                <div class="row-fluid banner-wrapper" data-store="banner-home-categories">
+                    <div class="span4">
+                        <div class="textbanner text-center">
+                            <a class="banner-with-text-link" href="https://wakko.mitiendanube.com/bazar/" title="Bazar" aria-label="Bazar">
+                                <div class="textbanner-image overlay">
+                                    <img class="textbanner-image-background lazyautosizes lazyload blur-up-big" src="https://agusc01.github.io/api-wakko/assets/images/banner/4432.jpg" data-sizes="auto" data-expand="-10" alt="Bazar"> 
+                                    <div class="placeholder-shine"></div>
+                                </div>
+                                <div class="textbanner-text">
+                                    <div class="textbanner-title">Bazar</div>
+                                    <div class="textbanner-paragraph">Productos Nacionales !</div>
+                                    <div class="btn btn-primary">Ver más productos</div>
+                                </div>
+                            </a>
+                        </div>
                     </div>
-                </div>`;
+                    <div class="span4">
+                        <div class="textbanner text-center">
+                            <a class="banner-with-text-link" href="https://wakko.mitiendanube.com/estancia-don-juan/" title="Estancia Don Juan" aria-label="Estancia Don Juan">
+                                <div class="textbanner-image overlay">
+                                    <img class="textbanner-image-background lazyautosizes lazyload blur-up-big" src="https://agusc01.github.io/api-wakko/assets/images/banner/4432.jpg" data-sizes="auto" data-expand="-10" alt="Estancia Don Juan"> 
+                                    <div class="placeholder-shine"></div>
+                                </div>
+                                <div class="textbanner-text">
+                                    <div class="textbanner-title">Estancia Don Juan</div>
+                                    <div class="textbanner-paragraph">Productos Nacionales !</div>
+                                    <div class="btn btn-primary">Ver más productos</div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>    
+                </div>               
+                `
+                ;
             // TODO: Take off jQuery . jQuery is on tiendanube by default
-            $(".banner-wrapper").append(newCategory);
+            $(".banner-wrapper > .span6").closest("div").insertAfter(newCategory);
             categories.forEach(category =>{
                 category.classList.add("span4");
                 category.classList.remove("span6");
